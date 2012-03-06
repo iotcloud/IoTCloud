111869fed42a2042333c8427cf5085574d74c09c
supunkamburugamuva <supun06@gmail.com>
2012-03-06 09:28:08 -0500

    adding the listener input stream

--- modules/streaming-server/src/main/java/cgl/iotcloud/streaming/http/listener/HttpListener.java
+++ modules/streaming-server/src/main/java/cgl/iotcloud/streaming/http/listener/HttpListener.java
@@ -1,6 +1,10 @@
 package cgl.iotcloud.streaming.http.listener;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.ChannelGroupFuture;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
 import java.net.InetSocketAddress;
@@ -11,22 +15,37 @@ public class HttpListener {
 
     private MessageReceiver receiver;
 
+    private String path;
+
+    private ChannelGroup allChannels = null;
+
+    private NioServerSocketChannelFactory channelFactory = null;
+
     public HttpListener(int port, MessageReceiver receiver) {
         this.port = port;
         this.receiver = receiver;
+        allChannels = new DefaultChannelGroup("http-server-" + path + ":" + port);
     }
 
     public void start() {
-        // Configure the server.
-        ServerBootstrap bootstrap = new ServerBootstrap(
-                new NioServerSocketChannelFactory(
+        NioServerSocketChannelFactory channelFactory = new NioServerSocketChannelFactory(
                         Executors.newCachedThreadPool(),
-                        Executors.newCachedThreadPool()));
+                        Executors.newCachedThreadPool());
+        // Configure the server.
+        ServerBootstrap bootstrap = new ServerBootstrap(channelFactory);
 
         // Set up the event pipeline factory.
         bootstrap.setPipelineFactory(new HttpListenerPipelineFactory(receiver));
 
         // Bind and start to accept incoming connections.
-        bootstrap.bind(new InetSocketAddress(port));
+        Channel channel = bootstrap.bind(new InetSocketAddress(port));
+        allChannels.add(channel);
+    }
+
+    public void stop() {
+        ChannelGroupFuture future = allChannels.close();
+        future.awaitUninterruptibly();
+
+        channelFactory.releaseExternalResources();
     }
 }
