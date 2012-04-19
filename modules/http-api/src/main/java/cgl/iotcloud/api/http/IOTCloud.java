a894b9a3e0ac44a68314b65a92852e88f5b284e7
iotcloud <iotcloud@googlegroups.com>
2012-04-19 11:18:14 -0400

    rest easy

---
+++ modules/http-api/src/main/java/cgl/iotcloud/api/http/IOTCloud.java
@@ -0,0 +1,17 @@
+package cgl.iotcloud.api.http;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("/sgxservice")
+public class IOTCloud {
+
+    @GET
+    @Path("/init/{ip}")
+    @Produces("text/plain")
+    public String init(@PathParam("ip") String sgxIP){
+        return "Client already Initialized";
+    }
+}
