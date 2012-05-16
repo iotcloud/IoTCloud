52c112dfca53c47bce813a62dad9fc07d1204e24
supunkamburugamuva <supun06@gmail.com>
2012-05-16 09:10:16 -0400

    adding client rest services

--- modules/http-api/src/main/java/cgl/iotcloud/api/http/ClientServices.java
+++ modules/http-api/src/main/java/cgl/iotcloud/api/http/ClientServices.java
@@ -8,6 +8,7 @@ import cgl.iotcloud.core.sensor.SCSensorUtils;
 import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
@@ -28,6 +29,23 @@ public class ClientServices {
                 Constants.IOT_CLOUD_SERVLET_PROPERTY);
 
         List<SCSensor> sensorList = iotCloud.getSensorCatalog().getSensors();
+
+        Response.ResponseBuilder r = status(200);
+
+        String sensors = SCSensorUtils.convertToString(sensorList);
+        r = r.entity(sensors);
+
+        return r.build();
+    }
+
+    @GET
+    @Path("/sensors/{id}")
+    @Produces("text/xml")
+    public Response getSensor(@PathParam("id") String id) {
+        IoTCloud iotCloud = (IoTCloud) servletContext.getAttribute(
+                Constants.IOT_CLOUD_SERVLET_PROPERTY);
+
+        SCSensor sensorList = iotCloud.getSensorCatalog().getSensor(id);
 
         Response.ResponseBuilder r = status(200);
 
