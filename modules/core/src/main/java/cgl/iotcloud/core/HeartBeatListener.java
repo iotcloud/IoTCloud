1a5728f96e5f426c5c80276d71931a56926cb993
Supun Kamburugamuva <supun06@gmail.com>
2012-07-12 14:32:13 -0400

    adding heartbeat listener

---
+++ modules/core/src/main/java/cgl/iotcloud/core/HeartBeatListener.java
@@ -0,0 +1,10 @@
+package cgl.iotcloud.core;
+
+/**
+ * We will listen for the heartbeats sent by the sensors to figure out weather they are alive,
+ * The heartbeat interval will be sent to the sensor when they register to the sensor cloud.
+ * A sensor should send a status message periodically over the update channel to indicate its aliveness,
+ */
+public class HeartBeatListener {
+
+}
