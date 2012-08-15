4534da17967e0c4b090616a78d16f9be4b5bf2b1
Supun Kamburugamuva <supun06@gmail.com>
2012-08-15 16:21:57 -0400

    adding turtle sensor

--- modules/samples/turtlebot/src/main/java/cgl/iotcloud/samples/turtlebot/sensor/TurtleSensor.java
+++ modules/samples/turtlebot/src/main/java/cgl/iotcloud/samples/turtlebot/sensor/TurtleSensor.java
@@ -1,49 +1,61 @@
 package cgl.iotcloud.samples.turtlebot.sensor;
 
-import geometry_msgs.Twist;
-import org.ros.concurrent.CancellableLoop;
-import org.ros.namespace.GraphName;
-import org.ros.node.AbstractNodeMain;
-import org.ros.node.ConnectedNode;
-import org.ros.node.topic.Publisher;
+import cgl.iotcloud.core.Constants;
+import cgl.iotcloud.core.message.SensorMessage;
+import cgl.iotcloud.core.message.control.DefaultControlMessage;
+import cgl.iotcloud.core.sensor.AbstractSensor;
+import cgl.iotcloud.sensors.SensorAdaptor;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import org.ros.exception.RosRuntimeException;
+import org.ros.internal.loader.CommandLineLoader;
+import org.ros.node.DefaultNodeMainExecutor;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMain;
+import org.ros.node.NodeMainExecutor;
 
-public class TurtleSensor extends AbstractNodeMain {
-    @Override
-    public GraphName getDefaultNodeName() {
-        return GraphName.of("/controller");
+public class TurtleSensor extends AbstractSensor {
+    public TurtleSensor(String type, String name) {
+        super(type, name);
+    }
+
+    public static void main(String[] argv) throws Exception {
+        // register with ros_java
+        CommandLineLoader loader = new CommandLineLoader(Lists.newArrayList(argv));
+
+        final String nodeClassName = "cgl.iotcloud.samples.turtlebot.sensor.RosTurtle";
+        NodeConfiguration nodeConfiguration = loader.build();
+
+        NodeMain nodeMain = null;
+        try {
+            nodeMain = loader.loadClass(nodeClassName);
+        } catch (ClassNotFoundException e) {
+            throw new RosRuntimeException("Unable to locate node: " + nodeClassName, e);
+        } catch (InstantiationException e) {
+            throw new RosRuntimeException("Unable to instantiate node: " + nodeClassName, e);
+        } catch (IllegalAccessException e) {
+            throw new RosRuntimeException("Unable to instantiate node: " + nodeClassName, e);
+        }
+
+        Preconditions.checkState(nodeMain != null);
+        NodeMainExecutor nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
+        nodeMainExecutor.execute(nodeMain, nodeConfiguration);
+
+        // register the sensor itself
+        SensorAdaptor adaptor = new SensorAdaptor("http://localhost:8080");
+
+        TurtleSensor sensor = new TurtleSensor(Constants.SENSOR_TYPE_BLOCK, "turtle-sensor");
+        adaptor.registerSensor(sensor);
+
+        adaptor.start();
     }
 
     @Override
-    public void onStart(final ConnectedNode connectedNode) {
-        final Publisher<Twist> publisher =
-                connectedNode.newPublisher("cmd_vel", Twist._TYPE);
+    public void onControlMessage(SensorMessage message) {
+        if (message instanceof DefaultControlMessage) {
+            DefaultControlMessage controlMessage = (DefaultControlMessage) message;
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException ie) {
-            ie.printStackTrace();
+
         }
-
-        // This CancellableLoop will be canceled automatically when the node shuts
-        // down.
-        connectedNode.executeCancellableLoop(new CancellableLoop() {
-            private int sequenceNumber;
-
-            @Override
-            protected void setup() {
-                sequenceNumber = 0;
-            }
-
-            @Override
-            protected void loop() throws InterruptedException {
-                Twist str = publisher.newMessage();
-                str.getLinear().setX(.1);
-
-                str.getAngular().setZ(-1);
-
-                publisher.publish(str);
-                Thread.sleep(1000);
-            }
-        });
     }
 }
---
+++ modules/samples/turtlebot/src/main/java/cgl/iotcloud/samples/turtlebot/sensor/Velocity.java
@@ -0,0 +1,39 @@
+package cgl.iotcloud.samples.turtlebot.sensor;
+
+public class Velocity {
+    private int x;
+
+    private int y;
+
+    private int z;
+
+    public Velocity(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+}
