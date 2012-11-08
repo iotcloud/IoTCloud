523f9e7b4217e185c61885ac1d8dea7dec3ad2fa
apadiyar <apadiyar@indiana.edu>
2012-11-07 19:51:45 -0500

    adding file in turtlebot and renamed files

---
+++ modules/samples/common/src/main/java/cgl/iotcloud/client/robot/DataController.java
@@ -0,0 +1,6 @@
+package cgl.iotcloud.client.robot;
+
+public interface DataController {
+	void start(String sensorName);
+	void stop(String sensorName);
+}
---
+++ modules/samples/turtlebot/src/main/java/cgl/iotcloud/samples/turtlebot/common/TurtleSensorTypes.java
@@ -0,0 +1,6 @@
+package cgl.iotcloud.samples.turtlebot.common;
+
+
+public interface TurtleSensorTypes {
+	String KINECT_SENSOR = "Kinect"; 
+}
