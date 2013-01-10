572d09c48399fb66912d1079f5569fdb04313bca
Supun Kamburugamuva <supun06@gmail.com>
2013-01-10 11:51:33 -0500

    aa

--- modules/samples/arducopter/src/main/java/cgl/iotcloud/samples/arducopter/client/KeyControlListener.java
+++ modules/samples/arducopter/src/main/java/cgl/iotcloud/samples/arducopter/client/KeyControlListener.java
@@ -1,24 +1,18 @@
 package cgl.iotcloud.samples.arducopter.client;
 
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
-public class KeyControlListener implements KeyListener {
-    @Override
+public class KeyControlListener extends KeyAdapter {
     public void keyTyped(KeyEvent e) {
-        System.out.println("Key typed");
-        System.out.println(e.getKeyCode());
+
     }
 
-    @Override
     public void keyPressed(KeyEvent e) {
-        System.out.println("Key pressed");
-        System.out.println(e.getKeyCode());
+
     }
 
-    @Override
     public void keyReleased(KeyEvent e) {
-        System.out.println("key released");
-        System.out.println(e.getKeyCode());
+
     }
 }
--- modules/samples/arducopter/src/main/java/cgl/iotcloud/samples/arducopter/client/control/Controller.java
+++ modules/samples/arducopter/src/main/java/cgl/iotcloud/samples/arducopter/client/control/Controller.java
@@ -23,22 +23,23 @@ public class Controller {
     }
 
     public void move(StickPos s, Direction d) {
-        JoyStick stick;
+        JoyStick stick = null;
         if (s == StickPos.LEFT) {
             stick = left;
-        } else {
+        } else if (s == StickPos.RIGHT) {
             stick = right;
+        } else {
+            return;
         }
 
         if (d == Direction.UP) {
-            stick.change(Axis.Y, .1);
+            stick.change(Axis.Y, JoyStick.Action.INCR);
         } else if (d == Direction.DOWN) {
-            stick.change(Axis.Y, -.1);
+            stick.change(Axis.Y, JoyStick.Action.DECR);
         } else if (d == Direction.LEFT) {
-            stick.change(Axis.X, .1);
-        } else if (d == Direction.)
-
+            stick.change(Axis.X, JoyStick.Action.INCR);
+        } else if (d == Direction.RIGHT) {
+            stick.change(Axis.X, JoyStick.Action.DECR);
+        }
     }
-
-
 }
