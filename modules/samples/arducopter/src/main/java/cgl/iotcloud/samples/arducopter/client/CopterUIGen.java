62d00cf890d1a62cc047c6ef3b180386aa4058e0
supunkamburugamuva <supun06@gmail.com>
2013-01-16 16:17:50 -0500

    improving the code

---
+++ modules/samples/arducopter/src/main/java/cgl/iotcloud/samples/arducopter/client/CopterUIGen.java
@@ -0,0 +1,11 @@
+package cgl.iotcloud.samples.arducopter.client;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: supun
+ * Date: 1/16/13
+ * Time: 11:47 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CopterUIGen {
+}
